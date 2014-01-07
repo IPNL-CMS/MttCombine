f5a01953d32821d9ba2887f3ba7544d676a804ab
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2014-01-07 17:06:15 +0100

    Added utility to generate workspaces for systematics

---
+++ datacards/generateSystematicsWorkspaces.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Generate the workspaces associated with systematics datacards
+
+cd Systematics
+ln -s ../../workspaces .
+ln -s ../../workspaces_for_combined .
+
+mkdir workspaces_for_combined/Systematics
+
+for mass in 500 625 750 1000 1250 1500 2000; do
+  for syst in jecUp jecDown jerUp jerDown puUp puDown; do
+    output="datacard_${mass}_1+2btag_${syst}.txt"
+    text2workspace.py -m ${mass} $output -o workspaces_for_combined/Systematics/workspace_combined_${mass}_1+2btag_${syst}.root
+  done
+done

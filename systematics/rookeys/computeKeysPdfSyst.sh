6618424c28a7f1b119b8db553401a6f70f1b5e15
Anne-Laure Pequegnot <al.pequegnot@ipnl.in2p3.fr>
2014-04-14 16:36:05 +0200

    Correct the name of the scripts

--- systematics/rookeys/computeKeysPdfSyst.sh
+++ systematics/rookeys/computeKeysPdfSyst.sh
@@ -3,7 +3,7 @@
 echo "Usage: ./computeKeysPdfSyst [mass]"
 
 mass=$1
-datacard="../datacard_${mass}_1+2btag.txt"
+datacard="../../../datacards/datacard_${mass}_1+2btag.txt"
 
 output="keyspdf_syst_${mass}"
 mkdir $output
@@ -15,7 +15,7 @@ ln -s "../workspaces" .
 for i in {0..149}; do
   datacard_temp="/scratch/datacard_${mass}_1+2btag_${i}.txt"
 
-  sed "s/_workspace\.root/_workspace_${i}\.root/" $datacard > $datacard_temp
+  sed -e "s/_workspace\.root/_workspace_${i}\.root/" -e "s/zprime/Zprime/g" $datacard > $datacard_temp
 
   echo "Run combine for toy ${i}..."
   combine -M MaxLikelihoodFit --rMin -10 -S 0 -m $mass $datacard_temp > /dev/null 2>&1
